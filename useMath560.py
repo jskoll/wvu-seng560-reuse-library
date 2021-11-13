8134c9264dec061ea032cf90c901b8108bc8d06c
Jason Skollingsberg <js0327@mix.wvu.edu>
2021-11-13 17:51:04 -0500

    Simple reuse example

---
+++ useMath560.py
@@ -0,0 +1,25 @@
+"""
+This file is an attempt to resuse the code from
+https://github.com/zroberts/seng560-python-math/blob/main/src/zroberts-seng560-math/math560.py.
+
+To make it more simple to work with the code is incuded directly in this repo.  In real world use this would be 
+included via a package manager to automatically control including the library.
+"""
+
+import math560
+
+print("Using addition:")
+print(f"5 + 5 = {math560.addition(5, 5)}")
+print()
+
+print("Using subtract:")
+print(f"5 - 5 = {math560.subtract(5, 5)}")
+print()
+
+print("Using multiply:")
+print(f"5 * 5 = {math560.multiply(5, 5)}")
+print()
+
+print("Using divide:")
+print(f"5 / 5 = {math560.divide(5, 5)}")
+print()
